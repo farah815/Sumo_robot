4d70547f1ac76ad08b0c5f8c20bd327ff09261af
wateenwalid2 <wateenwalid2@gmail.com>
2026-07-26 03:44:08 +0300

    update launch file
    Removed comments in Arabic and cleaned up the code.

--- ROS_project/src/robo/launch/spawn.launch.py
+++ ROS_project/src/robo/launch/spawn.launch.py
@@ -4,15 +4,13 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    # 1. هات مسار الباكدج بطريقة مضمونة
-    pkg_share = get_package_share_directory('robo')
+       pkg_share = get_package_share_directory('robo')
     urdf_file = os.path.join(pkg_share, 'urdf', 'robot.urdf')
 
-    # 2. اقرا محتوى ملف الـ URDF مباشرة كنص XML
     with open(urdf_file, 'r') as infp:
         robot_description_content = infp.read()
 
-    # 3. عقدة نشر وصف الروبوت
+    
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -21,7 +19,7 @@ def generate_launch_description():
         }]
     )
 
-    # 4. عقدة إدخال الروبوت (Spawn)
+   
     spawn_robot = Node(
         package='ros_gz_sim',
         executable='create',
@@ -33,7 +31,7 @@ def generate_launch_description():
         output='screen'
     )
 
-    # 5. عقدة الـ Bridge
+    
     bridge_node = Node(
         package='ros_gz_bridge',
         executable='parameter_bridge',
