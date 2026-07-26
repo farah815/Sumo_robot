348b6cc3a177db6bdba748cb429a97e80fae6be8
habiba2335 <habiba2335@gmail.com>
2026-07-26 11:13:46 +0300

    Update sumo robot URDF with final sensor positions and components

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -5,14 +5,11 @@ from launch.substitutions import Command
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    # 1. تحديد اسم الباكدج ومسار ملف الـ URDF
     pkg_name = 'sumo_description'
     urdf_file = os.path.join(get_package_share_directory(pkg_name), 'urdf', 'sumo.urdf.xacro')
 
-    # 2. ترجمة ملف الـ Xacro لـ URDF يفهمه النظام
     robot_description = {'robot_description': Command(['xacro ', urdf_file])}
 
-    # 3. نود الـ Robot State Publisher (ده العقل اللي بيبعت تفاصيل الروبوت للمحاكاة)
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -20,14 +17,12 @@ def generate_launch_description():
         parameters=[robot_description]
     )
 
-    # 4. نود الـ Joint State Publisher GUI (ده اللي هيفتحلك شاشة البكرات عشان تحركي المفاصل بالماوس)
     node_joint_state_publisher_gui = Node(
         package='joint_state_publisher_gui',
         executable='joint_state_publisher_gui',
         name='joint_state_publisher_gui'
     )
 
-    # 5. نود الـ RViz (برنامج العرض الـ 3D نفسه)
     node_rviz = Node(
         package='rviz2',
         executable='rviz2',
